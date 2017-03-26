52ff66512c4940e133b71e8cd05009f9f0ef6e7f
GinkgoShare <cjw.elliott@gmail.com>
2017-03-26 11:42:09 -0400

    update functions to ES2015 format

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -14,20 +14,18 @@ function getRepos(username) {
 }
 
 function getTotalStars(repos) {
-  return repos.data.reduce(function(prev, current) {
-    return prev + current.stargazers_count
-  }, 0)
+  return repos.data.reduce((prev, current) => prev + current.stargazers_count, 0)
 }
 
 function getPlayersData(player) {
   return getRepos(player.login)
     .then(getTotalStars)
-    .then(function(totalStars) {
-      return {
+    .then((totalStars) => (
+      {
         followers: player.followers,
         totalStars: totalStars
       }
-    })
+    ))
 }
 
 function calculateScores(players) {
@@ -38,13 +36,10 @@ function calculateScores(players) {
 }
 
 export function getPlayersInfo(players) {
-  return axios.all(players.map(function(username) {
+  return axios.all(players.map((username) => {
     return getUserInfo(username)
-  })).then(function(info) {
-    return info.map(function(user) {
-      return user.data
-    })
-  }).catch(function(err) {
+  })).then((info) => info.map((user) => user.data)
+  ).catch(function(err) {
     console.warn("Error in getPlayersInfo", err)
   })
 }
@@ -55,7 +50,5 @@ export function battle(players) {
 
   return axios.all([playerOneData, playerTwoData])
     .then(calculateScores)
-    .catch(function(err) {
-      console.warn("Error in battle: ", err)
-    })
+    .catch((err) => console.warn("Error in battle: ", err))
 }
